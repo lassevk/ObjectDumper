6c5a9b1c03d355b5e4942c188ade13516dd5bd71
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2011-02-16 19:32:13 +0100

    Initial commit.

---
+++ ObjectDumper.Tests/ObjectDumperExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using NUnit.Framework;
+
+namespace ObjectDumper.Tests
+{
+    [TestFixture]
+    public class ObjectDumperExtensionsTests
+    {
+        public class Listener : TraceListener
+        {
+            public readonly StringBuilder Output = new StringBuilder();
+
+            public override void Write(string message)
+            {
+                Output.Append(message);
+            }
+
+            public override void WriteLine(string message)
+            {
+                Output.AppendLine(message);
+            }
+        }
+
+        [Test]
+        public void DumpToDebug_ReturnsSameInstancePassedToIt()
+        {
+            var obj = new object();
+            object output = obj.Dump();
+
+            Assert.That(output, Is.SameAs(obj));
+        }
+
+        [Test]
+        public void DumpToDebug_OutputsToDebug()
+        {
+            var obj = new object();
+            string name = Guid.NewGuid().ToString();
+
+            var listener = new Listener();
+            Debug.Listeners.Add(listener);
+            obj.Dump(name);
+            Debug.Listeners.Remove(listener);
+
+            Assert.That(listener.Output.ToString(), Is.StringContaining(name));
+        }
+    }
+}
---
+++ README.markdown
@@ -0,0 +1,15 @@
+This project contains code to dump an object and its contents
+for debugging purposes to a TextWriter, a file, the Debug output,
+or just get hold of it as a string.
+
+Repository and project location: [CodePlex][1]  
+Maintainer: [Lasse V. Karlsen][2]
+
+**NOTE**: This code is still very, *very*, much in flux. Do not use this
+in anything remotely related to production code. Expect methods to gain
+new parameters, be overloaded, or to disappear completely.
+
+**You have been warned!**
+
+  [1]: http://objectdumper.codeplex.com/
+  [2]: mailto:lasse@vkarlsen.no
